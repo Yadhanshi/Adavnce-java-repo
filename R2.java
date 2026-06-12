e64796a92bddc00df9d80a7a1b49c535a9356560
Yadhanshi Kawale <yadhanshi2005@gmail.com>
2026-06-12 17:42:30 +0530

    Count the paths

---
+++ R2.java
@@ -0,0 +1,21 @@
+public class R2 {
+public static int count(int i, int j,int n,int m)
+{
+    if(i ==n || j == m){
+        return 0;
+    }
+    if(i == n-1 && j == m-1){
+        return 1;
+    }
+    int downPaths = count(i+1, j, n, m);
+    int rightPaths = count(i, j+1, n, m);
+
+    return downPaths + rightPaths;
+}
+
+    public static void main(String[] args) {
+        int n = 3,m=3;
+int totalPaths = count(0,0, n, m);
+System.out.println(totalPaths);    
+}
+}
