c0157dfe23ba0cc1207274d08ed1caa277efead5
Yadhanshi Kawale <yadhanshi2005@gmail.com>
2026-06-24 22:08:44 +0530

    Diameter of Tree

---
+++ DiameterTree.java
@@ -0,0 +1,186 @@
+import java.util.*;
+public class DiameterTree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+
+        }
+    }
+    static class BinaryTree{
+        static int idx =-1;
+        public static Node buildTree(int nodes[])
+        {
+            idx++;
+            if(nodes[idx] ==-1){
+                return null;
+
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static void preorder(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        System.out.println(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(Node root)
+    {
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data+" ");
+        inorder(root.right);
+    }
+
+    public static void postorder(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.println(root.data+" ");
+
+    }
+
+    public static void levelorder(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty())
+        {
+            Node currNode = q.remove();
+            if(currNode == null)
+            {
+                System.out.println();
+                if(q.isEmpty())
+                {
+                    break;
+                }
+                else{
+                    q.add(null);
+                }
+            }
+            else{
+                System.out.println(currNode.data+" ");
+                if(currNode.left != null){
+                    q.add(currNode.left);
+
+                }
+ if(currNode.right != null){
+                    q.add(currNode.right);
+
+                }            }
+                
+        }
+    }
+
+    public static int sumNode(Node root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        int leftNodes = sumNode(root.left);
+         int rightNodes = sumNode(root.right);
+
+         return leftNodes + rightNodes + root.data;
+
+    }
+    public static int height(Node root)
+    {
+        if(root == null)
+        {
+                        return 0;
+
+        }
+        int leftHeight = height(root.left);
+                int rightHeight = height(root.right);
+
+                int myheight = Math.max(leftHeight, rightHeight)+1;
+                return myheight;
+
+    }
+
+    public static int diameter(Node root)
+    { if(root == null)
+    {
+        return 0;
+    }
+        int diam1 = diameter(root.left);
+             int diam2 = diameter(root.right);
+   int diam3 = height(root.left) + height(root.right) + 1;
+
+   return Math.max(diam3, Math.max(diam1, diam2));
+    }
+
+    static class TreeInfo{
+        int ht;
+        int diam;
+
+        TreeInfo(int ht, int diam)
+        {
+            this.ht = ht;
+            this.diam = diam;
+        }
+    }
+    public static TreeInfo diameter2(Node root)
+    {
+        if(root == null)
+        {
+            return new TreeInfo(0,0);
+        }
+        TreeInfo left = diameter2(root.left);
+        TreeInfo right = diameter2(root.right);
+
+        int myHeight = Math.max(left.ht, right.ht) + 1;
+
+        int diam1 = left.diam;
+                int diam2 = right.diam;
+        int diam3 = left.ht + right.ht + 1;
+
+        int mydiam = Math.max(Math.max(diam1, diam2),diam3);
+
+        TreeInfo myinfo = new TreeInfo(myHeight, mydiam);
+        return myinfo;
+
+    }
+    public static void main(String[] args) {
+        int nodes[] ={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root= tree.buildTree(nodes);
+        System.out.println(diameter2(root).diam);    
+}
+    
+}
+    
+
+     
+    
+
+    
