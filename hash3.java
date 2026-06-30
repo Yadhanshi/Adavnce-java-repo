3a34aa94b08b9dbbde3aac8c7b466718189f5c70
Yadhanshi Kawale <yadhanshi2005@gmail.com>
2026-06-30 23:29:55 +0530

    HashMap example

---
+++ hash3.java
@@ -0,0 +1,153 @@
+import java.util.*;
+
+public class hash3 {
+
+    static class HashMap<K, V> {
+
+        private class Node {
+            K key;
+            V value;
+
+            public Node(K key, V value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private int n; // total nodes
+        private int N; // bucket size
+        private LinkedList<Node> buckets[];
+
+        @SuppressWarnings("unchecked")
+        public HashMap() {
+            this.N = 4;
+            this.buckets = new LinkedList[N];
+
+            for (int i = 0; i < N; i++) {
+                buckets[i] = new LinkedList<>();
+            }
+        }
+
+        private int hashFunction(K key) {
+            int bi = key.hashCode();
+            return Math.abs(bi) % N;
+        }
+
+        private int searchInLL(K key, int bi) {
+            LinkedList<Node> ll = buckets[bi];
+
+            for (int i = 0; i < ll.size(); i++) {
+                if (ll.get(i).key.equals(key)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        @SuppressWarnings("unchecked")
+        private void rehash() {
+            LinkedList<Node> oldBucket[] = buckets;
+
+            N = N * 2;
+            buckets = new LinkedList[N];
+
+            for (int i = 0; i < N; i++) {
+                buckets[i] = new LinkedList<>();
+            }
+
+            n = 0;
+
+            for (int i = 0; i < oldBucket.length; i++) {
+                LinkedList<Node> ll = oldBucket[i];
+
+                for (int j = 0; j < ll.size(); j++) {
+                    Node node = ll.get(j);
+                    put(node.key, node.value);
+                }
+            }
+        }
+
+        public void put(K key, V value) {
+            int bi = hashFunction(key);
+            int di = searchInLL(key, bi);
+
+            if (di == -1) {
+                buckets[bi].add(new Node(key, value));
+                n++;
+            } else {
+                Node node = buckets[bi].get(di);
+                node.value = value;
+            }
+
+            double lambda = (double) n / N;
+            if (lambda > 2.0) {
+                rehash();
+            }
+        }
+
+        public boolean containsKey(K key) {
+            int bi = hashFunction(key);
+            int di = searchInLL(key, bi);
+
+            return di != -1;
+        }
+
+        public V remove(K key) {
+            int bi = hashFunction(key);
+            int di = searchInLL(key, bi);
+
+            if (di == -1) {
+                return null;
+            } else {
+                Node node = buckets[bi].remove(di);
+                n--;
+                return node.value;
+            }
+        }
+
+        public V get(K key) {
+            int bi = hashFunction(key);
+            int di = searchInLL(key, bi);
+
+            if (di == -1) {
+                return null;
+            } else {
+                Node node = buckets[bi].get(di);
+                return node.value;
+            }
+        }
+
+        public ArrayList<K> keySet() {
+            ArrayList<K> keys = new ArrayList<>();
+
+            for (int i = 0; i < buckets.length; i++) {
+                LinkedList<Node> ll = buckets[i];
+
+                for (int j = 0; j < ll.size(); j++) {
+                    Node node = ll.get(j);
+                    keys.add(node.key);
+                }
+            }
+
+            return keys;
+        }
+
+        public boolean isEmpty() {
+            return n == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("INDIA", 190);
+        map.put("US", 10);
+        map.put("China", 90);
+
+        ArrayList<String> keys = map.keySet();
+
+        for (int i = 0; i < keys.size(); i++) {
+            System.out.println(keys.get(i) + " " + map.get(keys.get(i)));
+        }
+    }
+}
