b8382bba56b057afe27f32b714c9ce8789834b39
Yadhanshi Kawale <yadhanshi2005@gmail.com>
2026-07-10 08:43:48 +0530

    TreeSet Example

---
+++ TreesetExample.java
@@ -0,0 +1,16 @@
+//Program 3.4
+
+import java.util.*;
+public class TreesetExample
+{
+    public static void main(String[] args)
+    {
+        TreeSet ts = new TreeSet();
+        ts.add("A");
+        ts.add("B");
+        ts.add("C");
+        ts.add("D");
+        ts.add("E");
+        System.out.println("The elements available in the tree set are : " + ts);
+    }
+}
