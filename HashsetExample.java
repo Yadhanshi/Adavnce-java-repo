3a2c497a8008b0a30eb010d72ad0038672a310f8
Yadhanshi Kawale <yadhanshi2005@gmail.com>
2026-07-10 08:42:15 +0530

    HashSet Example

---
+++ HashsetExample.java
@@ -0,0 +1,16 @@
+//Program 3.3
+import java.util.*;
+class HashsetExample
+{
+    public static void main(String[] args)
+    {
+        HashSet hs = new HashSet();
+        hs.add("D");
+        hs.add("A");
+        hs.add("C");
+        hs.add("B");
+        hs.add("E");
+        hs.add("C");
+        System.out.println("The elements available in the hash set are : " + hs);
+    }
+}
